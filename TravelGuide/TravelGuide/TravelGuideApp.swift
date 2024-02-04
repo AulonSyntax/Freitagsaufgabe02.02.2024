1c5ffff48a75d0597e2c4af19fe1a04daf0fff87
AulonSyntax <137051100+AulonSyntax@users.noreply.github.com>
2024-02-04 13:52:14 +0100

    Add files via upload

---
+++ TravelGuide/TravelGuide/TravelGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelGuideApp.swift
+//  TravelGuide
+//
+//  Created by Aulon Demolli on 02.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TravelGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelGuide/TravelGuideUITests/TravelGuideUITestsLaunchTests.swift
@@ -0,0 +1,7 @@
+//
+//  TravelGuideUITestsLaunchTests.swift
+//  TravelGuideUITests
+//
+//  Created by Aulon Demolli on 02.02.24.
+//
+
